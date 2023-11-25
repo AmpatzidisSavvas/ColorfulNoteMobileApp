b829226d569b6e5e7f43c5b5510cf3ecef3e4094
AmpatzidisSavvas <savvas.ampa@gmail.com>
2023-11-25 17:58:59 +0200

    First Commit

---
+++ app/src/main/java/gr/aueb/cf/colorfulnotesapp/helpers/DBHelper.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.colorfulnotesapp.helpers;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
+
+import java.util.Objects;
+
+public class DBHelper {
+
+    public static FirebaseDatabase databaseReference = FirebaseDatabase.getInstance();
+    public static FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    public static DatabaseReference getNotesReference() {
+        return databaseReference.getReference(getCurrentUserId()).child("notes");
+    }
+
+    public static String getCurrentUserId() {
+        return Objects.requireNonNull(auth.getCurrentUser()).getUid();
+    }
+}
